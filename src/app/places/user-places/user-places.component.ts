a56eaece31fe4005a2d8bd0783d712b035d519f5
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-27 19:39:43 +0300

    feat: get userPlaces data from the backend

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
+import type { Place } from '../place.model';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
 
@@ -11,4 +14,43 @@ import { PlacesComponent } from '../places.component';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.httpClient
+      .get('https://localhost:7067/api/user-places/all')
+      .pipe(
+        map((resData) => resData as Place[]),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. please try again later.'
+              )
+          );
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          //console.log(resData);
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
