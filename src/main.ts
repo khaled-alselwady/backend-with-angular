e23b7d3f6a5596f80922d4b9bc3e492a9fe978f3
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-28 03:17:40 +0300

    feat: use HTTP interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  const req = request.clone({
+    headers: request.headers.set('X-DEBUG', 'Testing'),
+  });
+
+  console.log('Outgoing request');
+  console.log(request);
+  return next(req);
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
 }).catch((err) => console.error(err));
